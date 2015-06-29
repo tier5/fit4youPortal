0cbd051158b024ac9a330dc5d428b511f6e53d91
rajkumarunified <rajkumar.unified@gmail.com>
2015-06-29 21:00:13 +0530

    Web services commit

--- src/Controller/WebservicesController.php
+++ src/Controller/WebservicesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Cake\Controller\Controller;
 use Cake\Event\Event;
 use Cake\Utility\Sendemail;
+use Cake\I18n\Time;
 
 // src/Controller/UsersController.php
 
@@ -11,7 +12,7 @@ class WebservicesController extends AppController
 {
 	var $uses=array('User');
 
-
+	
 
 	////////// login page
 
@@ -30,42 +31,154 @@ class WebservicesController extends AppController
 		$this->loadModel('UserRelations');
 		$pin = $this->request->data['pin'];
 		
-		//$query = $this->Users->find('all');
-		//$datas = $query->leftJoin(['ur' => 'user_relations'],['ur.client_id = Users.id'],['Users.userPin' => $pin]);
-		//$user_details=$query->first();	
-
-		$r=$this->UserRelations->find('all',array('conditions'=>array('Client.userPin' => $pin),'contain'=>['Client','Trainer']));
-		$arr = $r->toArray();
-		$data = $arr[0]->toArray();
 		
-		echo json_encode($data);
-		exit;
+				
+		if ($this->Users->find('all',['conditions' => ['userPin' => $pin,'role' => 'CLIENT', 'is_active' => '1']])->count())
+		{
+			$client_data = $this->Users->find('all',['conditions' => ['userPin' => $pin]])->first()->toArray();
+			
+			$query = $this->Users->query();
+			$flag = $query->update()
+				->set(['is_login' => '1'])
+				->where(['userPin' => $pin])
+				->execute();
+			
+						
+			$query = $this->UserRelations->query();
+			$flag = $query->update()
+				->set(['status' => '1'])
+				->where(['client_id' => $client_data['id'],'start_time <=' => date('Y-m-d H:i:s',strtotime('+1 hours')) ])
+				->execute();
+			
+			$query = $this->UserRelations->query();
+			$flag = $query->update()
+				->set(['status' => '0'])
+				->where(['client_id' => $client_data['id'],'end_time <' => date('Y-m-d H:i:s') ])
+				->execute();
+			
+			
+			$r=$this->UserRelations->find('all',array('conditions'=>array('Client.userPin' => $pin, 'Client.is_active' => '1','UserRelations.status' => '1'),'contain'=>['Client','Trainer']));
+			$arr = $r->toArray();
+			if(empty($arr))
+			{
+				$data['client'] = $client_data;
+				$data['client']['photo'] = BASE_URL.'uploads/images/users_profile/thumb/'.$client_data['photo'];
+				$data['client']['msg'] = 'You dont have any existing session';
+				//cho '<pre>';print_r($data);exit;
+				echo json_encode($data);
+				exit;
+			}
+			$data = $arr[0]->toArray();
+			
+			if($data['id'])
+			{
+				$start_time = get_object_vars($data['start_time']);
+				$end_time = get_object_vars($data['end_time']);
+				$data['status'] = '1';
+				$data['start_time'] =  $start_time['date'];
+				$data['end_time'] =  $end_time['date'];
+				$data['client']['photo'] = BASE_URL.'uploads/images/users_profile/thumb/'.$data['client']['photo'];
+				$data['trainer']['photo'] = BASE_URL.'uploads/images/users_profile/thumb/'.$data['trainer']['photo'];
+				//echo '<pre>';print_r($data);exit;
+				echo json_encode($data);
+				exit;
+			}
+			else
+			{
+				echo json_encode(['status'=>0,'msg'=>'Sorry there is some error.Try later']);
+				exit;
+				
+			}
+		}
+		else
+		{
+			echo json_encode(['status'=>0,'msg'=>'Sorry PIN does not match']);
+			exit;	
+		}
+		
 	}
 	
 	public function loginTrainer()
 	{
+		
 		$this->autoRender = false;
 		$this->loadModel('Users');
 		$this->loadModel('UserRelations');
 		$pin = $this->request->data['pin'];
+
 		
-		$r=$this->UserRelations->find('all',array('conditions'=>array('Trainer.userPin' => $pin),'contain'=>['Trainer','Client']));
-		$arr = $r->toArray();
-		$data = $arr[0]->toArray();
-		echo json_encode($data);
-		exit;
+		if ($this->Users->find('all',['conditions' => ['userPin' => $pin,'role' => 'TRAINER', 'is_active' => '1']])->count())
+		{
+			$trainer_data = $this->Users->find('all',['conditions' => ['userPin' => $pin]])->first()->toArray();
+			
+			$query = $this->Users->query();
+			$flag = $query->update()
+				->set(['is_login' => '1'])
+				->where(['userPin' => $pin])
+				->execute();
+			
+			$query = $this->UserRelations->query();
+			$flag = $query->update()
+				->set(['status' => '1'])
+				->where(['trainer_id' => $trainer_data['id'],'start_time <=' => date('Y-m-d H:i:s',strtotime('+1 hours')) ])
+				->execute();
+				
+			$query = $this->UserRelations->query();
+			$flag = $query->update()
+				->set(['status' => '0'])
+				->where(['trainer_id' => $trainer_data['id'],'end_time <' => date('Y-m-d H:i:s'), ])
+				->execute();
+				
+			$r=$this->UserRelations->find('all',array('conditions'=>['Trainer.userPin' => $pin, 'Trainer.is_active' => '1','UserRelations.status' => '1'],'contain'=>['Trainer','Client']));
+			$arr = $r->toArray();
+			if(empty($arr))
+			{
+				
+				$data = $this->Users->find('all',['conditions' => ['userPin' => $pin]])->first()->toArray();
+				$data['photo'] = BASE_URL.'uploads/images/users_profile/thumb/'.$data['photo'];
+				$data['msg'] = 'You dont have any existing session';
+				echo json_encode($data);
+				exit;
+			}
+			$data = $arr[0]->toArray();
+			//echo '<pre>';print_r($data);echo '<pre>';exit;
+			if($data['id'])
+			{
+				$start_time = get_object_vars($data['start_time']);
+				$end_time = get_object_vars($data['end_time']);
+				$data['status'] = '1';
+				$data['start_time'] =  $start_time['date'];
+				$data['end_time'] =  $end_time['date'];
+				$data['client']['photo'] = BASE_URL.'uploads/images/users_profile/thumb/'.$data['client']['photo'];
+				$data['trainer']['photo'] = BASE_URL.'uploads/images/users_profile/thumb/'.$data['trainer']['photo'];
+				echo json_encode($data);
+				exit;
+			}
+			else
+			{
+				echo json_encode(['status'=>0,'msg'=>'Sorry there is some error.Try later']);
+				exit;
+				
+			}
+		}
+		else
+		{
+			echo json_encode(['status'=>0,'msg'=>'Sorry PIN does not match']);
+			exit;
+			
+		}
 	}
 	
 	public function present()
 	{
 		$this->autoRender = false;
 		$this->loadModel('Users');
-		$user_id = $this->request->data['user_id'];
+		$pin = $this->request->data['pin'];
 		
 		$query = $this->Users->query();
 		$flag = $query->update()
 				->set(['is_present' => '1'])
-				->where(['id' => $user_id])
+				->where(['pin' => $pin])
 				->execute();
 
 		if(!empty($flag))
