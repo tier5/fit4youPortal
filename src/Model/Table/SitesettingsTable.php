81603cee6f0f7479e65b20a41144430a3dd2931f
rajkumarunified <rajkumar.unified@gmail.com>
2015-06-30 20:51:47 +0530

    Adminsetting Table  commit

---
+++ src/Model/Table/SitesettingsTable.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Organisation;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Organisations Model
+ */
+class SitesettingsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        $this->table('sitesettings');
+        $this->displayField('id');
+        $this->primaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->add('id', 'valid', ['rule' => 'numeric'])
+            ->allowEmpty('id', 'create');
+
+        return $validator;
+    }
+}
